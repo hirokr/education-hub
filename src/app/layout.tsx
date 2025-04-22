d96d4927c9fc6ee671a12d9517804f38143b50f5
hirokr <hrr3645@gmail.com>
2025-04-22 20:13:24 +0600

    hydration bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body className={`${openSans.className} antialiased`}>
         <ThemeProvider
           attribute='class'
